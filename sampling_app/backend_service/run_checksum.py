5a81a9f64641602bef0876feb925dfae1417d8d4
yugijimoh <dreamlin0930@gmail.com>
2020-12-03 01:19:13 +0800

    add minor changes to backend

--- sampling_app/backend_service/run_checksum.py
+++ sampling_app/backend_service/run_checksum.py
@@ -1,10 +1,16 @@
 import hashlib
 from urllib import parse, request
 import logging
-
+from pyspark import SparkContext
+logger = logging.getLogger()
+try:
+    sc.stop()
+except Exception as e:
+    logger.warning("sc.stop executed, no sc found.")
 
 logger = logging.getLogger()
-res_dict = {}
+error_spans = {}  # {traceid:[span1,span2...spann],}
+res_dict = {}  # {traceid:md5,}
 
 """
 several things that run checksum need to do.
@@ -23,9 +29,9 @@ several things that run checksum need to do.
 """
 
 
-def get_md5(traceid):
+def get_md5(span):
     m = hashlib.md5()
-    m.update(traceid)
+    m.update(span)
     return m.hexdigest()
 
 
@@ -39,4 +45,24 @@ def send_checksum():
 
 
 def run_checksum():
+    for trace_id, span in error_spans:
+        md5_value = get_md5(span)
+        res_dict[trace_id] = md5_value
+
+
+def update_error_spans(new_error_dict):
+    for trace_id, span in new_error_dict:
+        if error_spans[trace_id]:
+            error_spans[trace_id].extend(span)
+        else:
+            error_spans[trace_id] = span
+
+
+def sort_incoming_spans(spans):
+    """
+    :param spans: need to be transformed
+    rdd = sc.parallelize(spans)
+    rdd.sortBy(lambda x: x[1])
+    :return:
+    """
     pass
--- sampling_app/entrance.py
+++ sampling_app/entrance.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import request as req
 from client_service.run_client import run_client
 from backend_service.run_checksum import run_checksum
-import logging
+import logging,os
 logger = logging.getLogger()
 app = Flask(__name__)
-client_port1 = '8001'
-client_port2 = '8002'
-
+self_port = os.environ.get("SERVER_PORT")
+port = 8002
+print("self_port is {}".format(self_port))
+if self_port and len(self_port)==4:
+    port=self_port
 
 @app.route('/')
 def hello_world():
@@ -48,4 +50,4 @@ def ready4checksum():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=port)
